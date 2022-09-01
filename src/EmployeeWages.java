2758c93f380ca11650157491359cd8e63c245a8b
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 13:47:32 +0530

    Creating : Printing Welcome message in master branch

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -2,7 +2,7 @@ public class EmployeeWages {
     public static void main(String[] args) {
 
 
-
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
     }
 }
