e529e17b15b15136ad5269f5502fe94e3b77b0bc
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 13:52:17 +0530

    Refactored : Calculating Employee daily wages

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,17 +1,17 @@
 public class EmployeeWages {
     public static void main(String[] args) {
 
-
         {
 
+            int Wage_per_Hour = 20;
+            int Full_Day_Hour = 8;
             double empCheck = Math.floor(Math.random() * 10) % 2;
-
             if (empCheck == 1) {
-                System.out.println("Employee is present");
+                System.out.println("Daily Wages are : " + Wage_per_Hour * Full_Day_Hour);
             } else
-                System.out.println("Employee is absent");
+                System.out.println("Employee is Absent No wages were given");
 
         }
+
     }
 }
-
