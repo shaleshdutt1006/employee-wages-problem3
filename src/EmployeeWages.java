d2148f8c67eec56074f5eb4d95ca57b809b2b1d9
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 13:54:23 +0530

    Refactored : Calculating Employee Part time Full time Wages

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,15 +1,17 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-
         {
 
             int Wage_per_Hour = 20;
-            int Full_Day_Hour = 8;
+            int Full_Time = 8;
+            int Half_Time = 4;
+
             double empCheck = Math.floor(Math.random() * 10) % 2;
+
             if (empCheck == 1) {
-                System.out.println("Daily Wages are : " + Wage_per_Hour * Full_Day_Hour);
+                System.out.println("Employee is Full time his wages are : " + Wage_per_Hour * Full_Time);
             } else
-                System.out.println("Employee is Absent No wages were given");
+                System.out.println("Employee is Part time his wages are : " + Wage_per_Hour * Half_Time);
 
         }
 
