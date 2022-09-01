fe429724ff4b797744127b8aa418b07db3e7aebc
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 14:01:26 +0530

    Refactored : Calculating Wages for a month

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,28 +1,35 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-        {
-            System.out.println("Welcome to Employee Wage Computation Program");
-            int wagePerHour = 20;
-            int fullTimeHour = 8;
-            int partTimeHour = 4;
-            int dailyWage = 0;
-
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int wagePerHour = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+        int workingDays = 20;
+        int FullTimeWage = 0;
+        int PartTimewage = 0;
+        int absent = 0;
+        for (int day = 0; day < workingDays; day++) {
             int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
 
             switch (employeeCheck) {
                 case 2:
-                    dailyWage = fullTimeHour * wagePerHour;
-                    System.out.println("Employee is present full time");
+                    FullTimeWage = FullTimeWage + fullTimeHour * wagePerHour;
+
                     break;
                 case 1:
-                    dailyWage = partTimeHour * wagePerHour;
-                    System.out.println("Employee is present half time");
+                    PartTimewage = PartTimewage + partTimeHour * wagePerHour;
+
                     break;
                 default:
-                    System.out.println("Employee is absent");
+                    absent++;
+                    System.out.println("Employee is absent ");
             }
 
-            System.out.println("Employee total wage is: " + dailyWage);
         }
+        System.out.println("Employee is absent for " + absent);
+        System.out.println("Employee full time Wages are: " + FullTimeWage);
+        System.out.println("Employee Part time Wages are: " + PartTimewage);
+
+        System.out.println("Employee total wage for a month is: " + (FullTimeWage + PartTimewage));
     }
 }
